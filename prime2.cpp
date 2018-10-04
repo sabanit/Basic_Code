3f7cd7b0b1763b7f72c6253eadd1e873e53e2053
Anisha Kathuria <42143335+Anisha-Kathuria@users.noreply.github.com>
2018-10-04 20:47:31 +0530

    Update prime2.cpp

--- prime2.cpp
+++ prime2.cpp
@@ -1,23 +1,28 @@
+//To check if entered number is prime or not
 #include<iostream>
 using namespace std;
-int main()
+
+ int main()
 {
 	int n,flag=0;
 	cout<<"Enter number";
 	cin>>n;
-	if(n==1)
-	cout<<"Neither prime nor composite";
-	else
-	{
-	for(int i=2;i<n/2;i++)
-	{
+	  if(n==1)
+	   cout<<"Neither prime nor composite";
+	  else
+	  {
+	   for(int i=2;i<n/2;i++)
+	    {
 		if(n%i==0)
-		{flag=1; break; }
-	}
-	if(flag==0)
-	cout<<"Prime";
-	else
-	cout<<"Not prime";
-}
-	return 0;
+		{
+		  flag=1;                             //flag=1: not a prime number
+		  break; 
+		}
+	    }
+	   if(flag==0)
+	     cout<<"Prime";
+	   else
+	     cout<<"Not prime";
+          }
+        return 0;
 }
